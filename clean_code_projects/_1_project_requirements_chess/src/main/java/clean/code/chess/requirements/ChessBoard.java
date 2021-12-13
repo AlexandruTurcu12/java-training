9a9b65a7f063aac96ca8cc863c6e5ef3fbff3ae5
AlexandruTurcu12 <75173415+AlexandruTurcu12@users.noreply.github.com>
2021-12-13 20:37:58 +0200

    Update ChessBoard.java

--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -4,6 +4,8 @@ public class ChessBoard {
 
     public static int MAX_BOARD_WIDTH = 7;
     public static int MAX_BOARD_HEIGHT = 7;
+    private int[] BlackPawns = {0,0,0,0,0,0,0,0};
+    private int[] WhitePawns = {0,0,0,0,0,0,0,0};
 
     private Pawn[][] pieces;
 
@@ -13,10 +15,20 @@ public class ChessBoard {
     }
 
     public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+
+        if (xCoordinate == 6 && yCoordinate >= 0 && yCoordinate <= 7 && pieceColor == PieceColor.BLACK && BlackPawns[yCoordinate] == 0) {
+            pawn.setXCoordinate(xCoordinate);
+            pawn.setYCoordinate(yCoordinate);
+            BlackPawns[yCoordinate] = 1;
+        } else{
+            pawn.setXCoordinate(-1);
+            pawn.setYCoordinate(-1);
+        }
     }
 
     public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        if(xCoordinate < 0 || xCoordinate > 7 || yCoordinate < 0 || yCoordinate > 7)
+            return false;
+        return true;
     }
 }
